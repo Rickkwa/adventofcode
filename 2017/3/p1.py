416dc9d15d1fea86a0b8fea03263d52f2cf88ba5
Rickkwa <rickyk.162@gmail.com>
2017-12-03 18:03:09 -0500

    asd

--- 2017/3/p1.py
+++ 2017/3/p1.py
@@ -13,12 +13,19 @@ def main(inp):
     # Calculate the coordinate of the spiral where the odd numbered squared is
     coords = [level, level] # (1, 1) is in Q4
 
-    # Traverse to our input number (ie. only go up n + 1 times, then go left)
+    # Traverse the ring counter clockwise to our input number
     for i in range(n * n + 1, inp + 1):
-        if i <= n:
+        if i == n * n + 1: # The next ring starts 1 square to the right of odd squared number
+            coords[0] += 1
+        elif i <= n * n + n + 1:
             coords[1] -= 1
-        else:
+        elif i <= n * n + 2 * (n + 1):
             coords[0] -= 1
+        elif i <= n * n + 3 * (n + 1):
+            coords[1] += 1
+        elif i <= n * n + 4 * (n + 1):
+            coords[0] += 1
+
     return abs(coords[0]) + abs(coords[1])
 
 
@@ -33,5 +40,5 @@ if __name__ == "__main__":
     # print "== 26:", main(26)
     # print "== 27:", main(27)
     # print "== 28:", main(28)
-    # print "== 32:", main(32)
-    # print "== 35:", main(35)
+    print "== 32:", main(32)
+    print "== 35:", main(35)
