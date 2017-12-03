bfd3e8d6b6fbe92b32a49c2893414f76c8540597
Rickkwa <rickyk.162@gmail.com>
2017-12-02 22:45:06 -0500

    Revamp template

--- 2017/template/p1.py
+++ 2017/template/p1.py
@@ -1,11 +1,20 @@
 import sys
-input_file = "input1.txt"
-if len(sys.argv) > 1 and sys.argv[1].lower() in ["t", "--t", "test", "--test", "--check", "check"]:
-    input_file = "sample1.txt"
+import os
 
-
-with open(input_file, "r") as file:
-    for line in file:
+def main(fp):
+    for line in fp:
         line = line.strip()
 
 
+
+if __name__ == "__main__":
+    part = '1' if os.path.basename(__file__).endswith("1.py") else '2'
+
+    print "=== Part {0} ===".format(part)
+    print "=== Test ==="
+    with open("sample{0}.txt".format(part), "r") as file:
+        main(file)
+
+    print "=== Real ==="
+    with open("input{0}.txt".format(part), "r") as file:
+        main(file)
