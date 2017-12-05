b9e48fad3b4f18099da88c989eeddb3cc9e3fd55
Rickkwa <rickyk.162@gmail.com>
2017-12-05 00:30:52 -0500

    Day 5 and changes to aoc helpers

---
+++ 2017/5/p1.py
@@ -0,0 +1,36 @@
+import sys
+import os
+import math
+sys.path.append('../../lib')
+from aoc import *
+
+
+def main(inp):
+
+    nums = []
+    for line in inp:
+        nums.append(int(line))
+
+    count = 0
+    index = 0
+    while True:
+        old_index = index
+        index += nums[index]
+        count += 1
+        if index < 0 or index >= len(nums):
+            break
+        nums[old_index] += 1
+    return count
+
+
+
+
+if __name__ == "__main__":
+    part = '1' if os.path.basename(__file__).endswith("1.py") else '2'
+
+    print "=== Part {0} ===".format(part)
+    print "=== Test ==="
+    print main(read_lines("sample{0}.txt".format(part)))
+
+    print "=== Real ==="
+    print main(read_lines("input{0}.txt".format(part)))
--- lib/aoc.py
+++ lib/aoc.py
@@ -1,23 +1,23 @@
 def read_lines(filepath):
     # Read the file and return a list of stripped lines
-    result = []
     with open(filepath, "r") as file:
-        for line in file:
-            result.append(line.strip())
-    return result
+        return map(lambda s: s.strip(), file.readlines())
+
 
 def read_words(filepath, delim=None):
     # Read the file and return a list of lines split by the given character
-    result = []
     with open(filepath, "r") as file:
-        for line in file:
-            result.append(line.strip().split(delim))
-    return result
+        return map(lambda s: s.strip().split(delim), file.readlines())
+
 
 def read_ints(filepath):
     # Read the file and return a list of lines, split by space and casted to int
-    result = []
     with open(filepath, "r") as file:
-        for line in file:
-            result.append(map(lambda x: int(x), line.strip().split()))
-    return result
+        return map(lambda s: map(lambda x: int(x), list(s.split())), file.readlines())
+
+
+def read_int_list(filepath):
+    # The input file should contain a single int on each line. Return a list of ints.
+    with open(filepath, "r") as file:
+        return map(lambda s: int(s.strip()), file.readlines())
+
