65ddd3b55b0a9fe88e9f3b6682ada0847b7ab5d0
Rickkwa <rickyk.162@gmail.com>
2017-12-06 00:42:31 -0500

    Add day 6

---
+++ 2017/6/p2.py
@@ -0,0 +1,44 @@
+import sys
+import os
+import math
+sys.path.append('../../lib')
+from aoc import *
+
+
+
+def redistribute(nums):
+    max_index = nums.index(max(nums))
+    n = nums[max_index]
+    nums[max_index] = 0
+    index = max_index + 1
+    while n > 0:
+        nums[index % len(nums)] += 1
+        index += 1
+        n -= 1
+    return nums
+
+
+def main(inp):
+    count = 0
+    seen = []
+    while True:
+        inp = redistribute(inp)
+        count += 1
+        if ",".join(map(lambda x: str(x), inp)) in seen:
+            break
+        seen.append(",".join(map(lambda x: str(x), inp)))
+    return count - seen.index(",".join(map(lambda x: str(x), inp))) - 1
+
+
+
+if __name__ == "__main__":
+    part = '1' if os.path.basename(__file__).endswith("1.py") else '2'
+
+    # read_lines, read_words, read_ints, read_int_list
+
+    print "=== Part {0} ===".format(part)
+    print "=== Test ==="
+    print main(read_ints("sample{0}.txt".format(part))[0])
+
+    print "=== Real ==="
+    print main(read_ints("input{0}.txt".format(part))[0])
--- 2017/template/p1.py
+++ 2017/template/p1.py
@@ -14,6 +14,8 @@ def main(inp):
 if __name__ == "__main__":
     part = '1' if os.path.basename(__file__).endswith("1.py") else '2'
 
+    # read_lines, read_words, read_ints, read_int_list
+
     print "=== Part {0} ===".format(part)
     print "=== Test ==="
     print main(read_lines("sample{0}.txt".format(part)))
