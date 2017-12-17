1f60168e11b4469ca60433945e7bff9d285a4779
Rickkwa <rickyk.162@gmail.com>
2017-12-17 00:40:49 -0500

    Fix to add to after current pos instead at current pos

--- 2017/17/p1.py
+++ 2017/17/p1.py
@@ -9,10 +9,9 @@ def main(steps):
     buf = [0]
     pos = 0
     for i in range(2017):
-        pos = (pos + steps) % len(buf)
+        pos = (pos + steps) % len(buf) + 1
         buf.insert(pos, i + 1)
-        pos += 1
-
+    # print buf
     return buf[buf.index(2017) + 1]
 
 
